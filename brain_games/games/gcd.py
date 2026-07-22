4d92e7ed8132ce48e6fbd6384988e1675228c693
ivanart <arteyqzxc@gmail.com>
2026-07-23 00:51:20 +0300

    Add GCD game

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,13 @@
+import random
+from math import gcd
+
+MIN_NUMBER = 1
+MAX_NUMBER = 100
+
+
+def generate_round():
+    first_number = random.randint(MIN_NUMBER, MAX_NUMBER)
+    second_number = random.randint(MIN_NUMBER, MAX_NUMBER)
+    question = f'{first_number} {second_number}'
+    correct_answer = str(gcd(first_number, second_number))
+    return question, correct_answer
